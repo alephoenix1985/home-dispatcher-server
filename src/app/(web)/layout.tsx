bf5d8ebd73cb3cd4f66113ed707cbfffd9a07e9b
Alejandro Hernandez <alephoenix1985@gmail.com>
2025-12-28 19:52:47 +0100

    update 2

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.css';
+import {CoreProvider} from 'psf-core-next/providers/core.provider';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -12,7 +13,13 @@ export const metadata: Metadata = {
 export default function WebLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className={inter.className}>
-            {children}
+            <CoreProvider
+                translationConfig={{
+                    componentsToLoad: ['common', 'home'],
+                }}
+            >
+                {children}
+            </CoreProvider>
         </div>
     );
 }
